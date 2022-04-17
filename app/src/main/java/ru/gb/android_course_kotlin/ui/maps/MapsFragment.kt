13b4618c1be7fc6bab80f0afd358e1a3a5bdfdff
Pavelbour <pavelbour@gmail.com>
2022-04-17 13:22:44 +0300

    Builds

--- app/src/main/java/ru/gb/android_course_kotlin/ui/maps/MapsFragment.kt
+++ app/src/main/java/ru/gb/android_course_kotlin/ui/maps/MapsFragment.kt
@@ -13,6 +13,7 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import ru.gb.android_course_kotlin.BuildConfig
 import ru.gb.android_course_kotlin.R
 
 class MapsFragment : Fragment() {
@@ -42,7 +43,10 @@ class MapsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        var mapFragment: SupportMapFragment? = null
+        if (BuildConfig.FLAVOR == "premium") {
+            mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        }
         mapFragment?.getMapAsync(callback)
     }
 
